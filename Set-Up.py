c21de94b821ad845ce4643ceabdcbe6850258ead
Kamoga Henry <henryatubuntu@gmail.com>
2023-11-27 08:05:14 +0300

    Add files via upload

---
+++ Set-Up.py
@@ -0,0 +1,11 @@
+
+
+RFID and Raspberry Pi Connections : (BOARD Mode)
+
+SDA ===> Pin 24  
+SCK ===> Pin 23 
+MOSI ===> Pin 19  
+MISO ===> Pin 21
+GND ===> Pin 6  
+RST ===> Pin 22 
+3.3v ===> Pin 1
